e71bc4c73f7fceb40f31f6e1d23ff05d09d26411
Patrick McNally <Patrick.J.McNally@gmail.com>
2016-11-15 15:29:14 -0600

    Finish building models;

--- suorganizer/blog/models.py
+++ suorganizer/blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from organizer.models import Startup, Tag
 
 # Create your models here.
+class Post(models.Model):
+    title = model.CharField(max_length=63)
+    slug = models.SlugField(
+        max_length=63,
+        unique_for_month='pub_date',
+        help_text="A lable for URL config",
+    )
+    text = models.TestField()
+    pub_date = models.DateField(
+        'date published',
+        auto_now_add=True,
+    )
+    tags = models.ManyToManyField(Tag, related_name='blog_posts',)
+    startups = models.ManyToManyField(Startup, related_name='blog_posts',)
+
