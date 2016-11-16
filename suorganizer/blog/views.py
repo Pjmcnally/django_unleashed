b3beeabb570a283037a7e91363f93405895b624e
Patrick McNally <Patrick.J.McNally@gmail.com>
2016-11-16 17:50:57 -0600

    update blog views and urls

--- suorganizer/blog/views.py
+++ suorganizer/blog/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import View
+from django.views.decorators.http import require_http_methods
 
 from .models import Post
 
 
-def post_list(request):
-    return render(
-        request,
-        'blog/post_list.html',
-        {'post_list': Post.objects.all()}
-    )
+class PostList(View):
 
+    def get(self, request):
+        return render(
+            request,
+            'blog/post_list.html',
+            {'post_list': Post.objects.all()}
+        )
+
+@require_http_methods(['HEAD', 'GET'])
 def post_detail(request, year, month, slug):
     post = get_object_or_404(
         Post,
