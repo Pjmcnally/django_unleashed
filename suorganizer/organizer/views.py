aff5d153f0955c8a4b1779cc3d411c5dca67b024
Patrick McNally <Patrick.J.McNally@gmail.com>
2016-11-16 00:46:46 -0600

    Update views to render pager

--- suorganizer/organizer/views.py
+++ suorganizer/organizer/views.py
@@ -3,9 +3,14 @@ from django.http.response import HttpResponse
 
 from .models import Tag, Startup, NewsLink
 
+from django.template import Context, loader
+
 # Create your views here.
 def homepage(request):
     tag_list = Tag.objects.all()
-    output = ", ".join([tag.name for tag in tag_list])
+    template = loader.get_template('organizer/tag_list.html')
+    context = Context({'tag_list': tag_list})
+    print(context)
+    output = template.render(context)
     return HttpResponse(output)
 
