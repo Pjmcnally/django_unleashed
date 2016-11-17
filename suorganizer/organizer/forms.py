568227d4ce9c2ab8ab96049cae00cb76a3a9118c
Patrick McNally <Patrick.J.McNally@gmail.com>
2016-11-16 23:47:33 -0600

    add mixin method to follow DRY

--- suorganizer/organizer/forms.py
+++ suorganizer/organizer/forms.py
@@ -4,7 +4,7 @@ from django.core.exceptions import ValidationError
 from .models import Tag, Startup, NewsLink
 
 
-class TagForm(forms.ModelForm):
+class TagForm(SlugCleanMixin, forms.ModelForm):
     class Meta:
         model = Tag
         fields = '__all__'
@@ -12,26 +12,12 @@ class TagForm(forms.ModelForm):
     def clean_name(self):
         return self.cleaned_data['name'].lower()
 
-    def clean_slug(self):
-        new_slug = (
-            self.cleaned_data['slug'].lower()
-        )
-        if new_slug == 'create':
-            raise ValidationError('Slug may not be "create".')
-        return new_slug
 
-class StartupForm(forms.ModelForm):
+class StartupForm(SlugCleanMixin, forms.ModelForm):
     class Meta:
         model = Startup
         fields = '__all__'
 
-    def clean_slug(self):
-        new_slug = (
-            self.cleaned_data['slug'].lower()
-        )
-        if new_slug == 'create':
-            raise ValidationError('Slug may not be "create".')
-        return new_slug
 
 class NewsLinkForm(forms.ModelForm):
     class Meta:
