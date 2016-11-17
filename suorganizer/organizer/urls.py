376a52fdd3091d5ab7cdcb34d9a3c716c54c934c
Patrick McNally <Patrick.J.McNally@gmail.com>
2016-11-17 17:58:31 -0600

    add url path for tag_create

--- suorganizer/organizer/urls.py
+++ suorganizer/organizer/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 from .views import (
-    tag_detail, tag_list, startup_detail, startup_list
+    tag_detail, tag_list, startup_detail, startup_list, tag_create
 )
 
 urlpatterns = [
     url(r'^tag/$', tag_list, name='organizer_tag_list'),
+    url(r'^tag/create/$', tag_create, name='organizer_tag_create'),
     url(r'^tag/(?P<slug>[\w\-]+)/$', tag_detail, name='organizer_tag_detail'),
     url(r'^startup/$', startup_list, name='organizer_startup_list'),
     url(r'^startup/(?P<slug>[\w\-]+)/$', startup_detail, name='organizer_startup_detail'),
