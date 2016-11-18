40dd2b0f9a3be464d782c5a13e311351e30705a4
Patrick McNally <Patrick.J.McNally@gmail.com>
2016-11-18 17:22:32 -0600

    Add get_update_url function to post model

--- suorganizer/blog/models.py
+++ suorganizer/blog/models.py
@@ -39,3 +39,13 @@ class Post(models.Model):
                 'slug': self.slug,
             }
         )
+
+    def get_update_url(self):
+        return reverse(
+            'blog_post_update',
+            kwargs={
+                'year': self.pub_date.year,
+                'month': self.pub_date.month,
+                'slug': self.slug
+            }
+        )
