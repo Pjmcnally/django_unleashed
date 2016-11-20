df199ef519ac98d9704ace58f2f3a86fe605d05e
Patrick McNally <Patrick.J.McNally@gmail.com>
2016-11-19 23:21:41 -0600

    Add url to project urls to direct traffic to contact app.  Add ulr in contact app to direct traffic to appropriate view.

--- suorganizer/suorganizer/urls.py
+++ suorganizer/suorganizer/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 from blog import urls as blog_urls
+from contact import urls as contact_urls
 from organizer import urls as organizer_urls
 from .views import redirect_root
 
@@ -25,4 +26,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include(organizer_urls)),
     url(r'^blog/', include(blog_urls)),
+    url(r'^contact/', include(contact_urls)),
 ]
